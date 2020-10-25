949d6c7a6f92ef27a5368a7397459dffb359aa2c
webchick <abyron@webchick.net>
2020-10-25 01:20:39 -0700

    Capitalization. It's a thing.

--- sayhello.gd
+++ sayhello.gd
@@ -3,5 +3,5 @@ extends Panel
 func _ready():
 	get_node("Button").connect("pressed", self, "_on_Button_pressed")
 
-func _on_button_Pressed():
+func _on_Button_pressed():
 	get_node("Label").text = "HELLO!"
