b9c5547f6ff3a90fc3c52d3f5dbdd7ce250e5555
webchick <abyron@webchick.net>
2020-10-25 01:44:53 -0700

    Notify on certain events.

--- sayhello.gd
+++ sayhello.gd
@@ -6,3 +6,10 @@ func _ready():
 func _on_Button_pressed():
 	get_node("Label").text = "HELLO!"
 	get_tree().call_group("enemies", "player_was_discovered")
+
+func _notification(what):
+	match what:
+		NOTIFICATION_READY:
+			print("This is the same as overriding _ready()...")
+		NOTIFICATION_PROCESS:
+			print("This is the same as overriding _process()...")
