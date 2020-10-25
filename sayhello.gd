36e9f03328802bedf50dde7467aee9dba92c3829
webchick <abyron@webchick.net>
2020-10-25 02:00:15 -0700

    Programmatically add new node.

--- sayhello.gd
+++ sayhello.gd
@@ -1,11 +1,17 @@
 extends Panel
 
+var s;
+
 func _ready():
 	get_node("Button").connect("pressed", self, "_on_Button_pressed")
 
 func _on_Button_pressed():
 	get_node("Label").text = "HELLO!"
 	get_tree().call_group("enemies", "player_was_discovered")
+
+	s = Button.new()
+	s.set_rotation(30)
+	add_child(s)
 
 #func _notification(what):
 #	match what:
