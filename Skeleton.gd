8c1ff3ee537e02d9604b061ae00fb8d2dd187ca2
webchick <abyron@webchick.net>
2020-10-25 01:47:58 -0700

    Add more overridable functions

--- Skeleton.gd
+++ Skeleton.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
 func _ready():
+	print("ready")
 	add_to_group("enemies")
 
 func player_was_discovered():
 	print("UH OH.")
+
+func _enter_tree():
+	print("enter_tree")
+
+func _exit_tree():
+	print("exit_tree")
+
+func _process(delta):
+	print("process")
+
+func _physics_process(delta):
+	print("physics_process")
